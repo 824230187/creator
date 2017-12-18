e59c07f6c1b6c1dea86d6aa27ce90339930ac399
Creator <824230187@qq.com>
2017-12-18 10:11:43 +0800

    通过经纬度计算距离相关sql语句

--- creator-persistent/src/main/resources/sql/sensitive.sql
+++ creator-persistent/src/main/resources/sql/sensitive.sql
@@ -5,3 +5,13 @@ CREATE TABLE `key_word` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_key_word` (`key_word`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*通过经纬度计算距离*/
+ROUND(6378.138*2*ASIN(SQRT(POW(SIN((#{order.latitude}*PI()/180-oi.latitude*PI()/180)/2),2)+COS(#{order.latitude}*PI()/180)*COS(oi.latitude*PI()/180)*POW(SIN((#{order.longitude}*PI()/180-oi.longitude*PI()/180)/2),2)))*1000) AS distance
+/*根据距离筛选附近XXkm（range）*/
+ <![CDATA[
+    AND (oi.longitude < #{order.longitude} + #{order.range} and
+    oi.longitude > #{order.longitude} - #{order.range}) and
+    (oi.latitude < #{order.latitude} + #{order.range} and
+    oi.latitude > #{order.latitude} - #{order.range})
+]]>
